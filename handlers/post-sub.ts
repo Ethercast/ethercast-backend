49713c07861ca5b94a7f4e41933889ab315ad970
Salem <salmahmo@amazon.com>
2018-02-17 14:12:15 -0700

    improve validation of the address

--- handlers/post-sub.ts
+++ handlers/post-sub.ts
@@ -9,18 +9,27 @@ export const SubscriptionPostRequest = Joi.object().keys({
     .items(
       Joi.array()
         .items(
-          Joi.object().keys({
-            type: Joi.string().only('address', 'topic0', 'topic1', 'topic2', 'topic3').required(),
-            value: Joi.string().regex(/^0x[0-9a-f]{64}$/).required()
-          })
+          Joi.alternatives(
+            [
+              Joi.object().keys({
+                type: Joi.string().only('address', 'topic0', 'topic1', 'topic2', 'topic3').required(),
+                value: Joi.when(
+                  Joi.ref('type'),
+                  {
+                    is: 'address',
+                    then: Joi.string().regex(/^0x[0-9a-fA-F]{40}$/).required(),
+                    otherwise: Joi.string().regex(/^0x[0-9a-f]{64}$/).required()
+                  }
+                )
+              })
+            ]
+          )
         )
         .min(1)
         .max(10)
     )
     .required()
-    // at least 1 rule
     .min(1)
-    // no more than 100 rules
     .max(10)
 });
 
