98caaf717e19b5c4f084a13138d84a4b149d50fc
Moody Salem <moody.salem@gmail.com>
2018-02-28 23:32:28 -0500

    get it back to building

--- src/util/env.ts
+++ src/util/env.ts
@@ -2,8 +2,10 @@ import * as env from 'env-var';
 import { LogLevel } from 'bunyan';
 
 export const LOG_LEVEL: LogLevel = env.get('LOG_LEVEL').required().asString() as LogLevel;
+
 export const SUBSCRIPTIONS_TABLE: string = env.get('SUBSCRIPTIONS_TABLE').required().asString();
 export const WEBHOOK_RECEIPTS_TABLE: string = env.get('WEBHOOK_RECEIPTS_TABLE').required().asString();
-export const QUEUE_ARN: string = env.get('QUEUE_ARN').required().asString();
+
+export const NOTIFICATION_TOPIC_NAME: string = env.get('NOTIFICATION_TOPIC_NAME').required().asString();
 
 export const TOKEN_ISSUER: string = env.get('TOKEN_ISSUER').required().asString();
