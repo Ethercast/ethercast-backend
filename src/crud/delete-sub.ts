ea35c9da6229c8a0396fbebd4aeeaff3bc42c944
Moody Salem <moody.salem@gmail.com>
2018-03-01 21:37:30 -0500

    remove sns sub

--- src/crud/delete-sub.ts
+++ src/crud/delete-sub.ts
@@ -2,6 +2,9 @@ import 'source-map-support/register';
 import crud from '../util/subscription-crud';
 import { default as createApiGatewayHandler, simpleError } from '../util/create-api-gateway-handler';
 import logger from '../util/logger';
+import * as SNS from 'aws-sdk/clients/sns';
+
+const sns = new SNS();
 
 export const handle = createApiGatewayHandler(
   async ({ pathParameters: { id }, user }) => {
@@ -15,6 +18,13 @@ export const handle = createApiGatewayHandler(
       );
     }
 
+    // remove the sns topic subscription
+    await sns.unsubscribe({
+      SubscriptionArn: subscription.subscriptionArn
+    }).promise();
+    logger.info({ subscription }, 'unsubscribed subscription arn');
+
+    // deactivate the subscription
     await crud.deactivate(subscription.id);
     logger.info({ subscription }, `deactivated subscription`);
 
