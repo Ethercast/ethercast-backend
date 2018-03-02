50229d4846a78333c2891b3c9e0d6d9fc5edc518
Salem <salmahmo@amazon.com>
2018-03-02 17:12:05 -0500

    clean up on egest

--- src/pipe/egest.ts
+++ src/pipe/egest.ts
@@ -1,100 +1,143 @@
 import 'source-map-support/register';
-import { Context, Handler, SNSEvent } from 'aws-lambda';
+import { Handler, SNSEvent } from 'aws-lambda';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import fetch from 'node-fetch';
 import { Log } from '@ethercast/model';
 import { SUBSCRIPTIONS_TABLE, WEBHOOK_RECEIPTS_TABLE } from '../util/env';
 import { Subscription } from '../util/models';
 import logger from '../util/logger';
-
-interface Response {
-  success: boolean;
-  status: number;
-}
+import * as Joi from 'joi';
+import uuid = require('uuid');
 
 const client = new DocumentClient();
 
-const lookup = async (subscriptionArn: string) => {
-  logger.info({subscriptionArn}, 'querying subscription');
-  const { Items } = await client.query({
-    TableName: SUBSCRIPTIONS_TABLE,
-    IndexName: 'BySubscriptionArn',
-    Limit: 1,
-    KeyConditionExpression: 'HashKey = :hkey',
-    ExpressionAttributeValues: { ':hkey': subscriptionArn },
-  }).promise();
-
-  logger.info({Items}, 'found subscription');
-  if (!Items || Items.length !== 1) {
-    logger.error({Items}, 'strange query result');
-    throw('strange query result, expected 1 item');
-  }
-
-  return Items[0] as Subscription;
-};
-
-const ping = async (subscription: Subscription, log: Log) => {
-  return fetch(subscription.webhookUrl, {
-    method: 'POST',
-    headers: {
-      'user-agent': 'ethercast',
-      'x-ethercast-subscription-id': subscription.id,
-    },
-    body: JSON.stringify(log),
-    timeout: 1000,
-  })
-    .then((response) => {
-      const status = response.status;
-      logger.info(`delivered event to ${subscription.webhookUrl}: ${status}`);
-      const success = status >= 200 && status < 300;
-      return { success, status };
-    }, (err) => {
-      logger.warn(err, `delivery failed to ${subscription.webhookUrl}: ${err.toString()}`);
-      return { success: false, status: 0 };
-    });
-};
-
-const logReceipt = async (subscription: Subscription, res: Response) => {
-  return client.put({
-    TableName: WEBHOOK_RECEIPTS_TABLE,
-    Item: {
-    }
-  }).promise()
-    .then(() => {
-      logger.info(`logged receipt for subscription:${subscription.id}, success:${res.success}`)
-    }, (err) => {
-      logger.warn(err, `failed to log receipt for subscription${subscription.id}`);
-    });
-};
-
-const egest = async (subscriptionArn: string, log: Log) => {
-  const subscription = await lookup(subscriptionArn);
-  const result = await ping(subscription, log);
-  await logReceipt(subscription, result);
-};
-
-export const handle: Handler = async (event: SNSEvent, context: Context) => {
+async function getSubscriptionWithArn(subscriptionArn: string): Promise<Subscription> {
   try {
-    const { Records: records } = event;
-    if (!records) throw new Error('missing records');
-    if (!Array.isArray(records)) throw new Error('records is not an array');
+    logger.info({ subscriptionArn }, 'querying subscription');
+
+    const { Items } = await client.query({
+      TableName: SUBSCRIPTIONS_TABLE,
+      IndexName: 'BySubscriptionArn',
+      Limit: 1,
+      KeyConditionExpression: 'subscriptionArn = :subscriptionArn',
+      ExpressionAttributeValues: { ':subscriptionArn': subscriptionArn },
+    }).promise();
+
+    if (!Items || Items.length !== 1) {
+      logger.error({ Items }, 'unexpected query result');
+      throw new Error('unexpected query result');
+    }
+
+    return Items[0] as Subscription;
   } catch (err) {
-    throw new Error(`${err}: ${event}`);
+    logger.error({ err }, 'failed to get subscription by arn');
+    throw err;
+  }
+}
+
+interface NotificationReceipt {
+  success: boolean;
+  responseStatus: number;
+  receiptId: string;
+}
+
+async function notifyEndpoint(subscription: Subscription, log: Log): Promise<NotificationReceipt> {
+  // we create this here to correlate logs from sending the webhook with the receipt we store in dynamo
+  const receiptId = uuid.v4();
+
+  try {
+    logger.debug(
+      { receiptId, subscriptionId: subscription.id },
+      'notifying subscription'
+    );
+
+    const response = await fetch(
+      subscription.webhookUrl,
+      {
+        method: 'POST',
+        headers: {
+          'user-agent': 'ethercast',
+          'x-ethercast-subscription-id': subscription.id,
+          'x-ethercast-receipt-id': receiptId
+        },
+        body: JSON.stringify(log),
+        timeout: 1000
+      }
+    );
+
+    const status = response.status;
+
+    logger.info({
+      receiptId,
+      subscriptionId: subscription.id,
+      responseStatus: status
+    }, `delivered event`);
+
+    const success = status >= 200 && status < 300;
+
+    return { success, responseStatus: status, receiptId };
+  } catch (err) {
+    logger.error({ err, log, subscription, receiptId }, `failed to send log to subscription`);
+
+    return { success: false, responseStatus: 0, receiptId };
+  }
+}
+
+async function saveReceipt(subscription: Subscription, receipt: NotificationReceipt) {
+  try {
+    logger.debug({ subscription, receipt }, 'saving receipt');
+
+    await client.put({
+      TableName: WEBHOOK_RECEIPTS_TABLE,
+      Item: {
+        id: receipt.receiptId,
+        subscriptionId: subscription.id,
+        receipt: {}
+      }
+    }).promise();
+
+    logger.debug({ subscription, receipt }, 'saved receipt');
+  } catch (err) {
+    logger.error({ err, subscription, receipt }, `failed to save receipt`);
+    throw err;
+  }
+}
+
+async function sendLogNotification(subscriptionArn: string, log: Log) {
+  const subscription = await getSubscriptionWithArn(subscriptionArn);
+  const receipt = await notifyEndpoint(subscription, log);
+  await saveReceipt(subscription, receipt);
+}
+
+const JoiSnsNotification = Joi.object({
+  Records: Joi.array().items(
+    Joi.object({
+      EventSubscriptionArn: Joi.string().required(),
+      Sns: Joi.object({
+        Message: Joi.string().required()
+      }).required()
+    })
+  ).required()
+});
+
+export const handle: Handler = async (event: SNSEvent) => {
+  // Make sure it's an SNS message
+  const { value, error } = JoiSnsNotification.validate(event, { allowUnknown: true });
+
+  if (error) {
+    logger.error({ error }, 'sns event failed joi validation');
+    throw new Error('sns event failed joi validation');
   }
 
-  for (let i = 0; i < event.Records.length; ++i) {
-    const record = event.Records[i];
+  for (let i = 0; i < value.Records.length; ++i) {
+    const { EventSubscriptionArn, Sns: { Message } } = value.Records[i];
+
     try {
-      const { EventSubscriptionArn: subscriptionArn } = record;
-      if (!subscriptionArn) throw new Error('missing subscription arn');
-
-      const { Sns: { Message: message } } = record;
-      if (!message) throw new Error('missing message');
-
-      const log = JSON.parse(message);
-      await egest(subscriptionArn, log);
+      const log = JSON.parse(Message);
+      await sendLogNotification(EventSubscriptionArn, log);
     } catch (err) {
-      logger.error({record}, err.toString());
+      logger.error({ err, record: value.Records[i] }, 'failed to send log notification');
+      throw err;
     }
   }
 };
